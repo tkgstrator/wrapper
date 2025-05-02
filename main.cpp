fd8b2254036aa9f2cc01a3ee3a8d3fc8d7938668
WorldObservationLog <wolc@duck.com>
2025-05-02 16:18:23 +0800

    feat: exit when raise exception

--- main.cpp
+++ main.cpp
@@ -10,16 +10,19 @@ extern "C" uint8_t handle_cpp(int fd) {
         return 1;
     } catch (const std::exception &e) {
         fprintf(stderr, "[!] catched an exception: %s\n", e.what());
+        exit(1);
         return 0;
     }
 }
 
 static void endLeaseCb(int const &c) {
     fprintf(stderr, "[.] end lease code %d\n", c);
+    exit(1);
 }
 
 static void pbErrCb(void *) {
     fprintf(stderr, "[.] playback error\n");
+    exit(1);
 }
 
 extern "C" std::function<void (int const&)> endLeaseCallback(endLeaseCb);

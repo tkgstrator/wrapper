f6aa890218ee0fe937bfde8e350a1168766165fd
WorldObservationLog <wolc@duck.com>
2025-07-18 04:54:16 +0800

    fix: try to fix unable tu run on Linux

--- wrapper.c
+++ wrapper.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 
 #include <errno.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -9,15 +10,54 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/capability.h>
 
 #include "cmdline.h"
 
 pid_t child_proc = -1;
 struct gengetopt_args_info args_info;
+#define CAP_SYS_ADMIN_IDX 21
+#define CAP_SYS_ADMIN_BIT (1ULL << CAP_SYS_ADMIN_IDX)
 
 static void intHan(int signum) {
     if (child_proc != -1) {
         kill(child_proc, SIGKILL);
+    }
+}
+
+int has_cap_sys_admin() {
+    FILE *fp;
+    char line[256];
+    unsigned long long cap_eff = 0;
+    int found_cap_eff = 0;
+
+    fp = fopen("/proc/self/status", "r");
+    if (fp == NULL) {
+        return 0;
+    }
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (strncmp(line, "CapEff:", 7) == 0) {
+            char *value_str = line + 7;
+            while (*value_str == '\t' || *value_str == ' ') {
+                value_str++;
+            }
+            cap_eff = strtoull(value_str, NULL, 16);
+            found_cap_eff = 1;
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    if (!found_cap_eff) {
+        return 0;
+    }
+
+    if (cap_eff & CAP_SYS_ADMIN_BIT) {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
@@ -39,6 +79,13 @@ int main(int argc, char *argv[], char *envp[]) {
     mknod("/dev/urandom", S_IFCHR | 0666, makedev(0x1, 0x9));
     chmod("/system/bin/linker64", 0755);
     chmod("/system/bin/main", 0755);
+
+    if (has_cap_sys_admin()) {
+        if (unshare(CLONE_NEWPID)) {
+            perror("unshare");
+            return 1;
+        }
+    }
 
     child_proc = fork();
     if (child_proc == -1) {
